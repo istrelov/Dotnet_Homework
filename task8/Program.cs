b05659bae2af3a7f43c07ac5350e3d7c4990c51e
AleksandrIstrelov <istrelov004@gmail.com>
2022-12-23 20:42:06 +0300

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.Write("Введите число ");
+int LastNumb = int.Parse(Console.ReadLine() ?? "");
+int index = 1;
+
+if (LastNumb < index)
+{
+    Console.WriteLine("Число должно быть больше или равно 1");
+}
+else
+{
+    bool FirstPrint = true;
+    while (index <= LastNumb)
+    {
+        if((index%2) == 0)
+        {
+            if (FirstPrint)
+            {
+                Console.Write(index);
+                FirstPrint = false;
+            }
+            else
+            {
+                Console.Write($", {index}");
+            }
+        }
+
+        index++;
+    }
+}
