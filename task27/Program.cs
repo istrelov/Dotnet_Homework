25b7de24bce55df885f538e6c94bcfe3ebab6dd0
AleksandrIstrelov <istrelov004@gmail.com>
2023-01-18 01:57:30 +0300

    task 27 relise

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+// --------------Начало программы ----------------------
+
+Console.Clear();
+
+int UserNumber = InitUserNumber("Введите целое число: ", "Ошибка ввода числа");
+int SumResult = SumResultUserNumber(UserNumber);
+Console.WriteLine($"{UserNumber} -> {SumResult}");
+
+//-----------------Описание методов --------------------
+
+int InitUserNumber(string Message, string ErrorMessage)
+{
+    Console.Write(Message);
+
+    while(true)
+    {
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int UserNumber);
+
+        if(IsCorrect) 
+            return UserNumber;
+
+        Console.WriteLine(ErrorMessage);    
+    }
+}
+
+int SumResultUserNumber(int UserNumber)
+{
+    int sum = 0;
+    int AbsUserNumber = Math.Abs(UserNumber);
+
+    while(AbsUserNumber > 0)
+    {
+        sum = sum + AbsUserNumber % 10;
+        AbsUserNumber = AbsUserNumber / 10;
+    }
+
+    return sum;
+}
+
+void PrintResult(int UserNumber, int SumResult)
+{
+    Console.WriteLine($"{UserNumber} -> {SumResult}");
+}
