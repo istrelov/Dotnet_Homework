0411743be4891b0053e54b64e56a75498148f326
AleksandrIstrelov <istrelov004@gmail.com>
2023-01-22 01:28:29 +0300

    task34 result

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// --------------Начало программы ----------------------
+
+int size = GetUserNumber("Введите длину массива: ", "Ошибка ввода");
+int[] array = GetNewRandomArray(size, 100, 999);
+int ResCount = CountEvenArray(array);
+
+PrintResult(array, ResCount);
+
+//-----------------Описание методов --------------------
+
+int GetUserNumber(string Mess, string ErrorMess)
+{
+    Console.WriteLine(Mess);  
+    while(true)
+    {
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int num);   
+
+        if(IsCorrect)
+            return num;
+        
+        Console.WriteLine(ErrorMess); 
+    } 
+
+}
+
+int[] GetNewRandomArray(int size, int min, int max)
+{
+    int[] myArr = new int[size];
+    var rnd = new Random();
+    for(int index = 0; index < size; index++)
+    {
+        myArr[index] = rnd.Next(min, max + 1);   
+    }
+    return myArr;
+}
+
+int CountEvenArray(int[] array)
+{
+    int count = 0;
+
+    foreach (int item in array)
+    {
+        if(item % 2 == 0) count++;    
+    }
+
+    return count;
+
+}
+
+void PrintResult(int[] array, int ResCount)
+{
+    Console.WriteLine($"[{String.Join(" ", array)}] -> {ResCount}");
+}
+
