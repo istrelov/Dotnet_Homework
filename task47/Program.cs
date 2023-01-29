2476c71c6c8605f7ec4dcbc60381d847b422c1a9
AleksandrIstrelov <istrelov004@gmail.com>
2023-01-30 00:21:50 +0300

    task47 relise

--- task47/Program.cs
+++ task47/Program.cs
@@ -2,6 +2,53 @@
 
 // --------------Начало программы ----------------------
 
+int rows = GetUserNumber("Введите значение m ", "Ошибка ввода");
+int colums = GetUserNumber("Введите значение n ", "Ошибка ввода");
+float [,] array = GetNewUserArray(rows, colums, -9, 9);
 
-
+PrintArray(array);
 //-----------------Описание методов --------------------
+
+int GetUserNumber(string Mess, string ErrorMess)
+{
+    Console.WriteLine(Mess);
+    while (true)
+    {
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int num);
+
+        if (IsCorrect && num > 1)
+            return num;
+
+        Console.WriteLine(ErrorMess);
+    }
+
+}
+
+float[,] GetNewUserArray(int rows, int colums, int minvalue, int maxvalue)
+{
+    float[,] resarray = new float[rows, colums];
+    var Rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            resarray[i, j] = (float)Rand.NextDouble() * Rand.Next(minvalue, maxvalue+1);
+        }
+    }
+
+    return resarray;
+}
+
+void PrintArray(float [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            //округление до сотых]
+            Console.Write($"{Math.Round(arr[i, j],2)} ");
+        }
+        Console.WriteLine();
+    }
+}
