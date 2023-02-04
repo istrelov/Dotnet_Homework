8324f86edbfbfbd131fc4f555360a4c40b469ea5
AleksandrIstrelov <istrelov004@gmail.com>
2023-02-05 00:42:21 +0300

    task66 ok

---
+++ task66/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 69: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в 
+//промежутке от M до N.
+
+// --------------Начало программы ----------------------
+
+int M = GetUserNumber("Введите число M: ", "Ошибка ввода");
+int N = GetUserNumber("Введите число N: ", "Ошибка ввода");
+
+Console.WriteLine($"M = {M}; N = {N} -> ");
+Console.Write(GetSum(M, N));
+
+//-----------------Описание методов --------------------
+int GetUserNumber(string Mess, string ErrorMess)
+{
+    Console.WriteLine(Mess);
+    while(true)
+    {
+        bool IsCorrect = int.TryParse(Console.ReadLine(), out int num);
+
+        if (IsCorrect && num > 0)
+            return num;
+
+        Console.WriteLine(ErrorMess);
+    }
+
+}
+
+int GetSum(int M, int N)
+{
+    if(N == 1) return M;
+    else 
+    {   
+         
+        return GetSum(M + N, N-1);
+    }
+}
